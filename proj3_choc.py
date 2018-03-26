f33505707e6f3337df48b636eb053c256aede0f1
Anndo Ko <handcuffanndo@gmail.com>
2018-03-26 10:45:50 -0400

    Create db and tables

--- proj3_choc.py
+++ proj3_choc.py
@@ -12,6 +12,66 @@ DBNAME = 'choc.db'
 BARSCSV = 'flavors_of_cacao_cleaned.csv'
 COUNTRIESJSON = 'countries.json'
 
+# Create db
+conn = sqlite3.connect(DBNAME)
+cur = conn.cursor()
+
+# Drop tables if they exist
+statement = '''
+    DROP TABLE IF EXISTS 'Bars';
+'''
+cur.execute(statement)
+statement = '''
+    DROP TABLE IF EXISTS 'Countries';
+'''
+cur.execute(statement)
+conn.commit()
+
+# -- Create tables: Bars --
+statement = '''
+    CREATE TABLE 'Bars' (
+        'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+        'Company' TEXT NOT NULL,
+        'SpecificBeanBarName' TEXT NOT NULL,
+        'REF' TEXT,
+        'ReviewDate' TEXT,
+        'CocoaPercent' ,
+        'CompanyLocation' TEXT,
+        'CompanyLocationId' INTEGER,
+        'Rating' ,
+        'BeanType' TEXT,
+        'BroadBeanOrigin' TEXT,
+        'BroadBeanOriginId' INTEGER
+    );
+'''
+try:
+    cur.execute(statement)
+    print("Execute the statement to create the table: Bars")
+except:
+    print("Failure.")
+conn.commit()
+
+# -- Create tables: Countries --
+statement = '''
+    CREATE TABLE 'Countries' (
+        'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+        'Alpha2' TEXT,
+        'Alpha3' TEXT,
+        'EnglishName' TEXT,
+        'Region' TEXT,
+        'Subregion' TEXT,
+        'Population' INTEGER,
+        'Area' REAL
+    );
+'''
+try:
+    cur.execute(statement)
+    print("Execute the statement to create the table: Countries")
+except:
+    print("Failure.")
+conn.commit()
+
+
 
 # Part 2: Implement logic to process user commands
 def process_command(command):
@@ -34,5 +94,5 @@ def interactive_prompt():
             continue
 
 # Make sure nothing runs or prints out when this file is run as a module
-if __name__=="__main__":
-    interactive_prompt()
+# if __name__=="__main__":
+#     interactive_prompt()
